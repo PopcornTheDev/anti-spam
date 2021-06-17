9a9173adcb9350f43f10d094bcb36d6b41ecc18c
Popcorn The Dev <79060182+Popcorn-The-Crappy-Dev@users.noreply.github.com>
2021-06-17 03:24:44 -0500

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,35 @@
+const config = require("../config.json");
+const usersMap = new Map();
+
+exports.run = async (client, message) => {
+    if (message.author.bot) return;
+    if (config["anti spam"].enabled === true) {
+        if (usersMap.has(message.author.id)) {
+            const userData = usersMap.get(message.author.id);
+            let msgCount1 = userData.msgCount;
+            usersMap.set(message.author.id, {
+                msgCount: msgCount1 + 1,
+                lastMessage: message,
+            });
+            setTimeout(() => {
+                const userData = usersMap.get(message.author.id);
+                let msgCount1 = userData.msgCount;
+                usersMap.set(message.author.id, {
+                    msgCount: msgCount1 - 1,
+                    lastMessage: message,
+                });
+            }, 5000);
+            if (msgCount1 > config["anti spam"].limit) {
+                message.delete()
+                message.channel.send(`Stop refrain from spamming in this discord, It could result in a warn`)
+            };
+        } else {
+            usersMap.set(message.author.id, {
+                msgCount: 2,
+                lastMessage: message,
+            });
+        }
+    } else {
+
+    }
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+
+
+client.events = new Discord.Collection();
+
+
+
+    fs.readdir('./events/', (err, files) => {
+        if (err) console.log(err);
+        files.forEach(file => {
+            let eventFunc = require(`./events/${file}`);
+            let eventName = file.split(".")[0];
+            client.on(eventName, (...args) => eventFunc.run(client, ...args));
+        });
+});
+
+
+
+
+
+
+
+
+client.login(config.main.token)
